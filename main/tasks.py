8c0c916722f902386fac54a74f093d8e3aefb1da
AliFreeGuy <alifreeguy@gmail.com>
2025-07-26 12:42:24 +0330

    feat : base start self client

--- main/tasks.py
+++ main/tasks.py
@@ -28,16 +28,22 @@ API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 SESSION_STRING = os.getenv('SESSION_STRING')
 DEBUG = os.getenv("BOT_DEBUG", "False").lower() in ["true", "1", "yes"]
-PROXY = None
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 REDIS_DB = int(os.getenv("REDIS_DB", 0))
 ADMINS = [int(admin) for admin in os.getenv("ADMINS", "").split(",") if admin]
+PROXY = {
+        "scheme": os.getenv("PROXY_SCHEME", "socks5"),
+        "hostname": os.getenv("PROXY_HOSTNAME", "127.0.0.1"),
+        "port": int(os.getenv("PROXY_PORT", "1080"))
+    }
+
 
 
 r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT , db=REDIS_DB , decode_responses=True)
-
 app = Celery('link_to_file' , backend=f'redis://{REDIS_HOST}:6379/6' , broker=f'redis://{REDIS_HOST}:6379/6')
+
+
 app.conf.update(
     task_serializer='json',
     result_serializer='json',
@@ -49,32 +55,20 @@ app.conf.update(
 
 @app.task(name='tasks',bind=True,default_retry_delay=1,soft_time_limit=43200)
 def extractor_task(self , data=None):
-    """
-    تسک اصلی که تنظیمات را از ردیس می‌خواند و پردازش را انجام می‌دهد.
-    """
-    logger.info(f"تسک با داده‌های زیر شروع شد: {data}")
+
+    logger.info(f"************ STARTING TASK ************")
 
     try:
-        # ✅ ۱. یک نمونه از کلاینت همزمان ردیس ایجاد می‌شود
         redis_client = SyncRedisCache()
-
-        # ✅ ۲. تنظیمات از ردیس خوانده می‌شود
         settings = redis_client.get_settings()
-        logger.info(f"تنظیمات با موفقیت از ردیس بارگیری شد: {settings}")
-
-        # می‌توانید از تنظیمات در ادامه کد استفاده کنید
-        uploader_username = settings.get('uploader_username')
-        if not uploader_username:
-            logger.warning("هشدار: یوزرنیم آپلودر در تنظیمات یافت نشد.")
-
-        # حلقه تست شما برای بررسی عملکرد
-        logger.info("شروع حلقه تست...")
-        for i in range(20):
-            logger.info(f"تکرار حلقه: {i}")
-            time.sleep(1)
-        logger.info("پایان حلقه تست.")
-
+    
+        self_client = Client('self-client-task' , api_id = API_ID , api_hash=API_HASH , session_string=SESSION_STRING , proxy = PROXY if DEBUG else None )
+        
+        with self_client as bot : 
+            bot.send_message('alifreeguy' , 'hi user mother fucker')
+            
+            
     except Exception as e:
-        logger.error(f"خطایی در حین اجرای تسک رخ داد: {e}", exc_info=True)
-        # در صورت بروز خطا، تسک را مجدداً برای تلاش دوباره زمان‌بندی می‌کنیم
-        self.retry(exc=e, countdown=60) # ۶۰ ثانیه بعد دوباره تلاش کن
+        logger.error(str(e))
+        # self.retry(exc=e, countdown=60) 
+        
